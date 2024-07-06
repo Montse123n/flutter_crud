c01f0ef4de50c63fb968e1bfc8e5cfd783860f95
Montse123n <garciamontse1974@gmail.com>
2024-07-05 23:47:59 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:crud_flutter/paginas/pagina_lista.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Notas',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: PaginaLista(),
+    );
+  }
+}
